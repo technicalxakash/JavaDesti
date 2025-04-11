1a10b13c7e6833cac6ca327f68a82c7bf04e041b
technicalxakash <akashr2468@gmail.com>
2025-04-11 14:45:00 +0530

    ECeption handling try with multi catch

--- src/www/Exception/www/TryWithMultipleCatch.java
+++ src/www/Exception/www/TryWithMultipleCatch.java
@@ -30,9 +30,6 @@ public class TryWithMultipleCatch {
 	            System.out.println("data added");
 	            System.out.println("array operation completed");
 	        } 
-	        catch (Exception e){
-	            System.out.println("generic exception");
-	        }
 	        catch (InputMismatchException e) {
 	            System.out.println("exception handle");
 	        }
@@ -57,4 +54,19 @@ enter num1
 enter num2
 0
 ArithmeticException is genarated
+
+if we put first generic catch block means it wiill give
+Exception in thread "main" java.lang.Error: Unresolved compilation problems: 
+	Unreachable catch block for InputMismatchException. It is already handled by the catch block for Exception
+	Unreachable catch block for ArithmeticException. It is already handled by the catch block for Exception
+	Unreachable catch block for NegativeArraySizeException. It is already handled by the catch block for Exception
+	Unreachable catch block for ArrayIndexOutOfBoundsException. It is already handled by the catch block for Exception
+	Unreachable catch block for Exception. It is already handled by the catch block for Exception
+
+	at www.Exception.www.TryWithMultipleCatch.main(TryWithMultipleCatch.java:36)
+
+
+
++++++SO first we need to specify the specific catch block and generic block++++++++++
+
 */
