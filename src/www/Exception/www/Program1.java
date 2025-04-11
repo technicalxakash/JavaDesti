eee08369ea20d37fd640ec169948712ec662aa22
technicalxakash <akashr2468@gmail.com>
2025-04-11 14:09:20 +0530

    ECeption handling try with single catch block

--- src/www/Exception/www/Program1.java
+++ src/www/Exception/www/Program1.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Program1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-//        System.out.println("Division operator is initated");
-//        System.out.println("enter num1");
-//        int n1 = sc.nextInt();
-//        System.out.println("enter num2");
-//        int n2 = sc.nextInt();
-//        int res = n1/n2;
-//        System.out.println(res);
-//        System.out.println("division completed");
+        System.out.println("Division operator is initated");
+        System.out.println("enter num1");
+        int n1 = sc.nextInt();
+        System.out.println("enter num2");
+        int n2 = sc.nextInt();
+        int res = n1/n2;
+        System.out.println(res);
+        System.out.println("division completed");
         System.out.println("Array operator initiated");
         System.out.println("enter array size");
         int size = sc.nextInt();
---
+++ src/www/Exception/www/SingleCatchBloack.java
@@ -0,0 +1,35 @@
+package www.Exception.www;
+
+import java.util.Scanner;
+
+public class SingleCatchBloack {
+	 public static void main(String[] args) {
+		 try {
+			 Scanner sc = new Scanner(System.in);
+		        System.out.println("Division operator is initated");
+		        System.out.println("enter num1");
+		        int n1 = sc.nextInt();
+		        System.out.println("enter num2");
+		        int n2 = sc.nextInt();
+		        int res = n1/n2;
+		        System.out.println(res);
+		        System.out.println("division completed");
+		        System.out.println("Array operator initiated");
+		        System.out.println("enter array size");
+		        int size = sc.nextInt();
+		        int[] arr = new int[size];
+		        System.out.println("enter the position to add data");
+		        System.out.println("enter the position ");
+		        int pos = sc.nextInt();
+		        System.out.println("enter the data");
+		        int val = sc.nextInt();
+		        arr[pos] =val;
+		        System.out.println("data added");
+		        System.out.println("array operation completed");
+		 }
+	 catch(Exception e) {
+		 System.out.println("Exception is handled");
+	 }
+	       
+	    }
+}
